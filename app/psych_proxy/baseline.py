8042949afc0a6ff35c55b7d03e84edb439caab94
Xavier-Frank <oduorfrancis134@gmail.com>
2026-02-13 22:23:10 +0300

    initial commit for digital twin psychological proxy

---
+++ app/psych_proxy/baseline.py
@@ -0,0 +1,10 @@
+def get_baseline(user_id):
+    return {
+        "avg_step_delay": 1.3,
+        "avg_session_time": 48,
+        "avg_corrections": 1.1,
+        "avg_retries": 0.3,
+        "avg_backtracks": 0.6,
+        "avg_pin_delay": 1.4,
+        "avg_amount_hesitation": 3.2
+    }
