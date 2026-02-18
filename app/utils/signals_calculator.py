61cf7df7eb86c7cca38aaf20d94d20b97991659c
Xavier-Frank <oduorfrancis134@gmail.com>
2026-02-18 19:34:40 +0300

    fixed crushing timestamp and prevented division by zero

--- app/utils/signals_calculator.py
+++ app/utils/signals_calculator.py
@@ -1,10 +1,18 @@
 # Time deviation
-def calculate_time_deviation(session: dict, baseline : dict):
-    return abs(session["timestamp"].hour - baseline["mean_time"]) / baseline["std_time"]
+from datetime import datetime
+
+
+def calculate_time_deviation(session: dict, baseline: dict):
+    session_time = datetime.fromisoformat(session["timestamp"]).hour
+    mean_time = baseline["mean_time"]
+    std_time = baseline["std_time"] if baseline["std_time"] != 0 else 1  # avoid division by zero
+
+    return abs(session_time - mean_time) / std_time
 
     # Amount deviation
 def calculate_amount_deviation(session: dict, baseline : dict):
-    return abs(session["amount"] - baseline["mean_amount"]) / baseline["std_amount"]
+    std_amount = baseline["std_amount"] if baseline["std_amount"] != 0 else 1
+    return abs(session["amount"] - baseline["mean_amount"]) / std_amount
 
     # Recipient familiarity
 
@@ -13,4 +21,5 @@ def calculate_recipient_familiarity(session: dict, baseline : dict):
 
     # Navigation speed deviation
 def calculate_speed_deviation(session: dict, baseline : dict):
-    return abs(session["navigation_time_ms"] - baseline["mean_nav_time"]) / baseline["std_nav_time"]
+    std_nav_time = baseline["std_nav_time"] if baseline["std_nav_time"] != 0 else 1
+    return abs(session["navigation_time_ms"] - baseline["mean_nav_time"]) / std_nav_time
